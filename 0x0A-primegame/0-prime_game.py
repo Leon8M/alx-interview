61629105c91ba32800616c24a695b7843af7bdde
Leon Munene <leonmunene@gmail.com>
2024-03-15 08:46:56 +0300

    ww

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,37 +1,61 @@
 #!/usr/bin/python3
-"""Prime Game"""
+""" Prime Game """
 
 
-def is_winner(x, nums):
-    """Return name of player that won most rounds"""
+def isprime(n):
+    """ Return prime number """
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
-    def generate_primes(n):
-        """Filters prime numbers"""
-        primes = []
-        sieve = [True] * (n + 1)
-        sieve[0] = sieve[1] = False
-        for p in range(2, n + 1):
-            if sieve[p]:
-                primes.append(p)
-                for i in range(p * p, n + 1, p):
-                    sieve[i] = False
-        return primes
 
-    def find_winner(n):
-        """This finds the winner"""
-        primes = generate_primes(n)
-        if len(primes) % 2 == 0:
-            return "Ben"
+def delete_numbers(n, nums):
+    """ Remove numbers - return zero """
+    for i in range(len(nums)):
+        if nums[i] % n == 0:
+            nums[i] = 0
+
+
+def isWinner(x, nums):
+    """ Return name of player that won
+    most rounds
+    """
+    nums.sort()
+    winner = False
+    Maria = 0
+    Ben = 0
+    for game in range(x):
+        # prints("game# ", game+1)
+        nums2 = list(range(1, nums[game] + 1))
+        # print("nums: ", nums2)
+        turn = 0
+        while True:
+            """
+            # monitor turns, uncomment to watch
+            if turn % 2 != 0:
+                print("Ben turn ")
+            else:
+                print("Maria turn ")
+            """
+            change = False
+            for i, n in enumerate(nums2):
+                # print("n: ", n, "i: ", i)
+                if n > 1 and isprime(n):
+                    delete_numbers(n, nums2)
+                    change = True
+                    turn += 1
+                    break
+            # print("movement: ". nums2)
+            if change is False:
+                break
+        if turn % 2 != 0:
+            Maria += 1
         else:
-            return "Maria"
-
-    winners = [find_winner(n) for n in nums]
-    maria_wins = winners.count("Maria")
-    ben_wins = winners.count("Ben")
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+            Ben += 1
+        # print("Maria: {}, Ben: {}".format(Maria, Ben))
+    if Maria == Ben:
         return None
+    if Maria > Ben:
+        return "Maria"
+    return "Ben"
